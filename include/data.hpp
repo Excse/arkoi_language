2290af9f9373896b6305ac69fe260638c81b270f
timo <behrendtimo@yahoo.de>
2024-10-10 14:32:59 +0200

    Quick backup for refactoring the code

---
+++ include/data.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <iostream>
+
+enum class Size : size_t {
+    BYTE = 8,
+    WORD = 16,
+    DWORD = 32,
+    QWORD = 64,
+};
+
+std::ostream &operator<<(std::ostream &os, const Size &size);
+
+size_t _size_to_bytes(const Size &size);
